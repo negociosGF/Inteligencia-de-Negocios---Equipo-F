a0ec6675ed9abf4ae278756f0355b50c835ba3a1
nick00las <bnegociosf@gmail.com>
2023-07-08 05:14:29 -0500

    Update KNN.py

--- src/despliegue/KNN.py
+++ src/despliegue/KNN.py
@@ -37,24 +37,24 @@ def app():
     X_test = X[split:]
     y_test = y[split:]
 
-    knn = KNeighborsClassifier(n_neighbors=15)
-    knn.fit(X_train, y_train)
-
-    # Datos predecidos 
-    st.write("Dataframe con los resultados predecidos")
-    df['Predicted_Signal'] = knn.predict(X)
-    st.write(df)
-
-    # Precisión del modelo
-    st.write("Precisión del modelo")
-    st.write(accuracy_score(knn.predict(X_test), y_test))
-
     # Rangos de fecha
     start_date = pd.to_datetime(st.date_input('Fecha de inicio', value=df.index.min())).tz_localize(df.index.tz)
     end_date = pd.to_datetime(st.date_input('Fecha de fin', value=df.index.max())).tz_localize(df.index.tz)
 
     # Filtra los datos según los rangos de fecha seleccionados
     filtered_df = df.loc[start_date:end_date]
+
+    knn = KNeighborsClassifier(n_neighbors=15)
+    knn.fit(X_train, y_train)
+
+    # Datos predecidos
+    st.write("Dataframe con los resultados predecidos")
+    filtered_df['Predicted_Signal'] = knn.predict(X.loc[start_date:end_date])
+    st.write(filtered_df)
+
+    # Precisión del modelo
+    st.write("Precisión del modelo")
+    st.write(accuracy_score(knn.predict(X_test), y_test))
 
     # Gráfica de la tasa de error vs. valor de K
     tasa_error = []
@@ -71,3 +71,8 @@ def app():
     plt.ylabel('Tasa de Error')
     st.pyplot(fig)
 
+    knn = KNeighborsClassifier(n_neighbors=19)
+    knn.fit(X_train, y_train)
+    pred = knn.predict(X_test)
+    st.write('CON K=19')
+    st.write(classification_report(y_test, pred))
